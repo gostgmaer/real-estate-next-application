1e308bd31dd8ca6d86c957f775e526b71928c6b8
Kishor Sarkar <kishor.sarkar@inadev.com>
2024-01-16 18:09:13 +0530

    updated

--- src/pages/dashboard/my-profile/index.js
+++ src/pages/dashboard/my-profile/index.js
@@ -1,10 +1,11 @@
 import Layout from "@/components/global/layout";
 import ChangePassword from "@/components/pages/auth/changePassword";
 import Profile from "@/components/pages/dashboard/profile";
+import { serverMethod } from "@/lib/helper/network";
 import { getSession } from "next-auth/react";
 import Head from "next/head";
 import Link from "next/link";
-
+import { parse } from "cookie";
 const Orders = (props) => {
   return (
     <Layout>
@@ -14,9 +15,7 @@ const Orders = (props) => {
       <div className="container mx-auto my-10 ">
         <div className="flex flex-col max-w-7xl bg-gray-50 bg-gray-950 p-10 m-auto rounded-lg">
           <div className=" flex justify-between w-full mb-10 uppercase">
-            <h1 className="text-2xl font-bold mb-4 text-left">
-              My Profile
-            </h1>
+            <h1 className="text-2xl font-bold mb-4 text-left">My Profile</h1>
             <Link
               href={"/dashboard/profile/change-password"}
               className="px-4 py-1 flex items-center bg-[#186aa5] text-white font-semibold rounded-lg transition-transform transform hover:scale-105 focus:outline-none focus:ring focus:border-[#186aad]"
@@ -25,7 +24,7 @@ const Orders = (props) => {
             </Link>
           </div>
           <div className=" bg-gray-50 py-10 rounded-xl">
-            <Profile/>
+            <Profile />
           </div>
         </div>
       </div>
@@ -58,31 +57,34 @@ export default Orders;
 export const getServerSideProps = async (ctx) => {
   const session = await getSession(ctx);
 
-  // if (!session) {
-  //   return {
-  //     redirect: {
-  //       destination: '/auth/signin',
-  //       permanent: false,
-  //     },
-  //   };
-  // } else {
-  //   // const cookies = parse(ctx.req.headers.cookie || '');
-  //   // const token = cookies["headerPayload"] + "." + cookies["signature"];
-  //   // const params = {
-  //   //   method: "get",
-  //   //   token: token
-  //   // }
-  //   // const result = await serverMethod(`/user/auth/profile`, params);
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth/signin",
+        permanent: false,
+      },
+    };
+  } else {
+    const cookies = parse(ctx.req.headers.cookie || "");
+    console.log(cookies);
+    const tokendata =
+      "bearer " + cookies["headerPayload"] + "." + cookies["signature"];
+    const params = {
+      method: "get",
+      header: {
+        Authorization: tokendata,
+      },
+    };
+    const result = await serverMethod(
+      `/authentication/user/current/profile`,
+      params
+    );
 
-  //   // const data = result.result
-  //   return {
-  //     props: {
-  //       session
-  //     },
-  //   };
-  // }
-
-  return {
-    props: {},
-  };
+    return {
+      props: {
+        session,
+        data: result,
+      },
+    };
+  }
 };
